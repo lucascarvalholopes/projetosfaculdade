874c957abeb0a59e4bed06eb592bfba86967cc74
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-04-01 14:07:11 -0300

    atualização js

---
+++ javaScript semana3/Calcular média.js
@@ -0,0 +1,15 @@
+// Curso de Engenharia de Software - UniEVANGÉLICA
+// Disciplina de Programação Web
+// Dev: Bruno Paiva Souza
+// DATA: 01/04/2023
+function calcularMedia(numeros) {
+    let soma = 0; // variável para armazenar a soma dos números
+    for (let i = 0; i < numeros.length; i++) {
+      soma += numeros[i]; // somando cada número do array na variável soma
+    }
+    return soma / numeros.length; // calculando e retornando a média
+  }
+  const numeros = [5, 7, 10, 2];
+  const media = calcularMedia(numeros);
+  console.log(`A média dos números é: ${media}`);
+    
---
+++ javaScript semana3/Encontrar a palavra mais longa.js
@@ -0,0 +1,18 @@
+// Curso de Engenharia de Software - UniEVANGÉLICA
+// Disciplina de Programação Web
+// Dev: Bruno Paiva Souza
+// DATA: 01/04/2023
+function encontrarPalavraMaisLonga(frase) {
+    let palavras = frase.split(' '); // separando as palavras da frase em um array
+    let palavraMaisLonga = ''; // variável para armazenar a palavra mais longa
+    for (let i = 0; i < palavras.length; i++) {
+      if (palavras[i].length > palavraMaisLonga.length) {
+        palavraMaisLonga = palavras[i];
+      }
+    }
+    return palavraMaisLonga;
+  }
+  const frase = 'A raposa marrom rápida saltou sobre o cachorro preguiçoso.';
+  const palavraMaisLonga = encontrarPalavraMaisLonga(frase);
+  console.log(`A palavra mais longa é: ${palavraMaisLonga}`);
+    
