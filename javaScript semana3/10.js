325ec8e0ff5d47a247fa081df494a15cc05e596f
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-04-01 14:16:17 -0300

    atualização js

---
+++ javaScript semana3/10.js
@@ -0,0 +1,12 @@
+// Curso de Engenharia de Software - UniEVANGÉLICA
+// Disciplina de Programação Web
+// Dev: Bruno Paiva Souza
+// DATA: 01/04/2023
+function calcularFatorial(n) {
+    if (n === 0 || n === 1) { // caso base
+      return 1;
+    } else { // caso recursivo
+      return n * calcularFatorial(n-1);
+    }
+  }
+  
