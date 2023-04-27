84d8e083209f2906c2476b4afdf7a5fad3e40c0d
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-04-27 19:48:04 -0300

    atualização

--- project_city/src/server.js
+++ project_city/src/server.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/',(req, res)=>{
+  return res.send('Página inicial')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port,()=>{
+  console.log(`Servidor aberto | http://localhost:${port}`)
+})
