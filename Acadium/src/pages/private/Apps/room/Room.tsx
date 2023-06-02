d19916eff69d649fb7bfd3bd7e29ac433171738d
lucascarvalholopes <lucascarvalholopes2@gmail.com>
2023-06-02 12:42:36 -0300

    atualização

---
+++ Acadium/src/pages/private/Apps/room/Room.tsx
@@ -0,0 +1,14 @@
+class Room {
+    name: string;
+    capacity: number;
+    location: string;
+  
+    constructor(name: string, capacity: number, location: string) {
+      this.name = name;
+      this.capacity = capacity;
+      this.location = location;
+    }
+  }
+  
+  export default Room;
+  
